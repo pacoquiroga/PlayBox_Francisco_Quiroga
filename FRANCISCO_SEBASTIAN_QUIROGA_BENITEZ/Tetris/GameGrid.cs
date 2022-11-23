42091e179e2927c95e00bc2b34c7200e03c81e5c
Francisco Quiroga <pacoquiroga33@gmail.com>
2022-11-23 08:55:20 -0500

    PlayBox Francisco Quiroga

---
+++ FRANCISCO_SEBASTIAN_QUIROGA_BENITEZ/Tetris/GameGrid.cs
@@ -0,0 +1,25 @@
+﻿namespace Tetris
+{
+    public class GameGrid
+    {
+        private readonly int[,] grid;
+        public int Rows { get; }
+        public int Columns { get; }
+
+        public int this[int r, int c]
+        {
+            get => grid[r, c];
+            set => grid[r, c] = value;
+        }
+
+        public GameGrid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            grid = new int[rows, columns];
+        }
+
+        public bool IsInside(int r, int c)
+    
+    }
+}
